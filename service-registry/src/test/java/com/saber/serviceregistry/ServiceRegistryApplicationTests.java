3b11e2c38bd908d8aa15f6b99b78d84744d64a21
saber azizi <saberazizi66@yahoo.com>
2021-02-10 02:44:17 +0330

    add service registry

---
+++ service-registry/src/test/java/com/saber/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saber.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
